28d541af463458f8ce2181aca90590477dd6decf
Yuri Chernushenko <y.chernushenko@gmail.com>
2024-10-23 11:16:05 +0200

    change logo

--- src/conf.py
+++ src/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Cloud Data Stack'
+project = ''
 copyright = '2024, Yuri Chernushenko'
 author = 'Yuri Chernushenko'
 
