0a2cdbbfb81a9a728f60365adce25e40a0614ec8
Yuri Chernushenko <y.chernushenko@gmail.com>
2025-09-29 08:58:19 +0200

    refactor

--- src/conf.py
+++ src/conf.py
@@ -21,8 +21,6 @@ html_context = {
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
